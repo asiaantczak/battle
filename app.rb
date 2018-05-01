2eec9be68ea1845d4f82ffef9be44b21ee901d11
George Sykes <georgesykes86@gmail.com>
2018-05-01 13:50:15 +0100

    Adds redirectfor play.erb

--- app.rb
+++ app.rb
@@ -2,13 +2,27 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "My session secret"
+  end
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    puts session[:player_1]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    p @player_1
+    p @player_2
     erb(:play)
   end
 
