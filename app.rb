dc4d10f9964ba118b7764101003b989d3611116e
Asia Antczak <aasiaantczak@gmail.com>
2018-05-01 14:23:34 +0100

    add player 2 points

--- app.rb
+++ app.rb
@@ -14,15 +14,12 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
-    puts session[:player_1]
     redirect '/play'
   end
 
   get '/play' do
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
-    p @player_1
-    p @player_2
     erb(:play)
   end
 
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,10 +1,16 @@
 feature 'Feature tests for Battle app' do
 
+before do
+  visit('/')
+  fill_in('player_1', with: 'Joe')
+  fill_in('player_2', with: 'Gordon')
+  click_button "Play"
+end
   scenario 'adds players names' do
-    visit('/')
-    fill_in('player_1', with: 'Joe')
-    fill_in('player_2', with: 'Gordon')
-    click_button "Play"
     expect(page).to have_content('Joe vs Gordon')
+  end
+
+  scenario 'shows player_2 Hit Points' do
+    expect(page).to have_content "100HP"
   end
 end
